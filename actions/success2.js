b8df9ac3da3cb1a2014ddf6eabd3bfd4d1473509
=Agnieszka Leszczynska <=leszczykiewicz@gmail.com>
2015-04-25 19:34:06 +0200

    zmiany pod lab6

---
+++ actions/success2.js
@@ -0,0 +1,34 @@
+
+	var helpers = require("../helpers");
+	var Queue = require("queuemanager");
+	var SQSCommand = require("../sqscommand");
+
+	var AWS_CONFIG_FILE = "./config.json";
+	var APP_CONFIG_FILE = "./app.json";
+
+
+var task = function(request, callback){
+	
+		var AWS = require("aws-sdk");
+		AWS.config.loadFromPath(AWS_CONFIG_FILE);		
+
+	//var s3 = new AWS.S3(); 
+
+	var appConfig = helpers.readJSONFile(APP_CONFIG_FILE);
+	var queue = new Queue(new AWS.SQS(), appConfig.QueueUrl);
+	var sqsCommand = new SQSCommand(queue);
+	queue.sendMessage(request.query.bucket+':'+request.query.key, function(err, data)
+	{
+		if(err) { callback(err); return; }
+		else {callback(null, data.MD5OfMessageBody)}
+	});
+	
+
+		
+}
+
+
+exports.action = task;
+
+
+
